20becf9d5205a1a0c096357fd1d823fd98e1acfd
LeoAnjil <140929169+LeoAnjil@users.noreply.github.com>
2023-10-01 15:49:07 +1100

    Function 3:F3
     Prints the list of students with total marks less than a certain threshold.

--- StudentStatistics.java
+++ StudentStatistics.java
@@ -4,68 +4,40 @@
  * @version (01/10/2023)
  */
 
-        this.assg1Mark = assg1Mark;
-        this.assg2Mark = assg2Mark;
-        this.ass3Mark = assg3Mark;
-        totalMarkCalculation();
-
-    }
-
-
-    private void TotalMarkCalculation() {
-        TotalMark = assg1Mark + assg2Mark + assg3Mark;
-    }
-}
-
-public class StudentStatistics
-{
-
-@@ -37,6 +41,7 @@ public static void main(String[] args) {
-        //allow user to enter the File Name
-        System.out.print("Please Enter the file name with valid Extension: ");
-        String fileName = scanner.nextLine();
-
-
-        try {
-            BufferedReader buff = new BufferedReader(new FileReader(fileName));
-@@ -59,7 +64,7 @@ private static Student[] readStudentDataFromFile(String fileName) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            int lineNumber = 0;
-            Student[] std = new Student[70]; 
-            Student[] students = new Student[70]; 
-
-            while ((line = reader.readLine()) != null) {
-                lineNumber++;
-@@ -84,13 +89,28 @@ private static Student[] readStudentDataFromFile(String fileName) {
-                        assg3Mark = Double.parseDouble(segment[5]);
-                    }
-
-                    std[lineNumber - 2] = new Student(lastName, firstName, studentID, ass1Mark, ass2Mark, ass3Mark);
-                    students[lineNumber - 2] = new Student(lastName, firstName, studentID, assg1Mark, assg2Mark, assg3Mark);
-                }
-            }
-            return std;
-            return students;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+     return null;
         }
+    }
+
+    private static double parseDouble(String value) {
+        try {
+            if (!value.isEmpty() && value.matches("[0-9.]+")) {
+                return Double.parseDouble(value);
+            }
+        } catch (NumberFormatException e) {
+            // exception handling
+        }
+        return 0.0; // default value
     }
     private static void studentTotalMarkCalculation(Student[] students) {
         System.out.println("Student list with their name, student ID, assessment marks and the total mark:");
         for (Student student : students) {
-            if (student != null) { 
-                // Checking if student is null
-                double totalMark = student.ass1Mark + student.ass2Mark + student.ass3Mark;
-                System.out.println("Student Name: " + student.firstName + " " + student.lastName +
-                    ", Student ID: " + student.studentID +
-                    ", Assignment 1 Mark: " + student.ass1Mark +
-                    ", Assignment 2 Mark: " + student.ass2Mark +
-                    ", Assignment 3 Mark: " + student.ass3Mark +
-                    ", Total Mark: " + totalMark);
+@@ -113,4 +124,19 @@ private static void studentTotalMarkCalculation(Student[] students) {
             }
         }
     }
 
+    private static void studentsBelowThreshold(Student[] students, double threshold) {
+        System.out.println("Total marks below  " + threshold + ":");
+        for (Student student : students) {
+            if (student != null) { 
+                // null check
+                double totalMark = student.ass1Mark + student.ass2Mark + student.ass3Mark;
+                if (totalMark < threshold) {
+                    System.out.println("Name: " + student.firstName + " " + student.lastName +
+                        ", Student ID: " + student.studentID +
+                        ", Total Mark: " + totalMark);
+                }
+            }
+        }
+    }
 }
